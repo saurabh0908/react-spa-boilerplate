74aacb30c4c0ac7d68f00f6abfefff4a0445fd45
Saurabh Shrivastava <saurabhshrivastava09@gmail.com>
2017-03-20 08:07:26 +0000

    Reverted back to react router 3.0.2, and made some link related changes

--- dev/index.jsx
+++ dev/index.jsx
@@ -1,22 +1,91 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+    Router,
+    Route,
+    hashHistory,
+    IndexRoute,
+    IndexLink,
+    Link
+  } from "react-router";
 
-class HelloWorld extends React.Component {
+let destination = document.getElementById('container');
+class App extends React.Component {
   render() {
     return(
-      <p>Hello. {this.props.greetTarget} !</p>
+      <div>
+        <h1>Simple SPA using React</h1>
+        <ul className="header">
+          <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
+          <li><Link to="/stuff" activeClassName="active">Stuff</Link></li>
+          <li><Link to="/contact" activeClassName="active">Contact</Link></li>
+        </ul>
+        <div className="content">
+             {this.props.children}
+        </div>
+      </div>
     );
   }
 }
 
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>Cras facilisis urna ornare ex volutpat, et
+        convallis erat elementum. Ut aliquam, ipsum vitae
+        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
+        metus nec massa. Maecenas hendrerit laoreet augue
+        nec molestie. Cum sociis natoque penatibus et magnis
+        dis parturient montes, nascetur ridiculus mus.</p>
+
+        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+      </div>
+    );
+  }
+}
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>GOT QUESTIONS?</h2>
+        <p>The easiest thing to do is post on
+        our <a href="http://our-travel-story.com">forums</a>.
+        </p>
+      </div>
+    );
+  }
+}
+
+class Stuff extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>STUFF</h2>
+        <p>Mauris sem velit, vehicula eget sodales vitae,
+        rhoncus eget sapien:</p>
+        <ol>
+          <li>Nulla pulvinar diam</li>
+          <li>Facilisis bibendum</li>
+          <li>Vestibulum vulputate</li>
+          <li>Eget erat</li>
+          <li>Id porttitor</li>
+        </ol>
+      </div>
+    );
+  }
+}
+
+
 ReactDOM.render(
-  <div>
-    <HelloWorld greetTarget="Batman"/>
-    <HelloWorld greetTarget="Iron Man"/>
-    <HelloWorld greetTarget="Spider Man"/>
-    <HelloWorld greetTarget="Super Man"/>
-    <HelloWorld greetTarget="Ant Man"/>
-    <HelloWorld greetTarget="He Man"/>
-  </div>,
-  document.getElementById('container')
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
+      <Route path="stuff" component={Stuff} />
+      <Route path="contact" component={Contact} />
+    </Route>
+  </Router>,
+  destination
 );
